91f44b6f779254d9ecab005ba51553aa8a8bfd1e
Elijah Hampton <elijahhampton@MBP-de-Elijah-2.lan>
2023-03-17 22:09:01 -0400

    Stubs required methods.

---
+++ protocol/contracts/Ubique.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.8.0;
+
+contract Ubique {
+    constructor() {}
+
+    function addBounty() public {}
+
+    function releaseBounty() public {}
+
+    function addBid() public {}
+
+    function removeBid() public {}
+
+    function updateBid() public {}
+
+    function placeBid() public {}
+
+    function releaseBids() public {}
+
+    function releaseBid() public {}
+
+    function addCid() public {}
+
+    function fund() public {}
+
+    function policyOK() public {}
+
+    function acceptBid() public {}
+
+    function authorizeData() public {}
+
+    function disputeClaimedBounty() public {}
+}
